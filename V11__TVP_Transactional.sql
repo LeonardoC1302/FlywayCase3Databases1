d72cabb7958f8c3a56942ac47d6d4d143c08e7f9
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-04 15:05:25 -0600

    TVP Fixed

--- V11__TVP_Transactional.sql
+++ V11__TVP_Transactional.sql
@@ -1,83 +1,51 @@
------------------------------------------------------------
--- Autor: Rnunez
--- Fecha: 1/5/2023
--- Descripcion: esta description en comentarios queda almacenada
--- Otros detalles de los parametros
------------------------------------------------------------
--- Author: Leonardo Céspedes y Frankmin Feng
--- Fecha: 04-26-2023
--- Desc: actualiza la tabla de xyz bla bla bla
------------------------------------------------------------
-
-CREATE TYPE ContractType AS TABLE (
-    startDate DATE NOT NULL,
-    endDate DATE NOT NULL,
-    contractStatusId INT NOT NULL
-);
-
-GO
-
-ALTER PROCEDURE [dbo].[InsertContract]
-    @contracts ContractType READONLY
-AS
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[prodCharges]') AND type in (N'P', N'PC'))
 BEGIN
-    SET NOCOUNT ON;
-    DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT;
-    DECLARE @Message VARCHAR(200);
-    DECLARE @InicieTransaccion BIT;
-
-
-	-- Check for NULL values before starting the transaction
-    IF EXISTS (SELECT 1 FROM @contracts WHERE startDate IS NULL OR endDate IS NULL OR contractStatusId IS NULL)
+    EXEC ('CREATE PROCEDURE [dbo].[prodCharges]
+        @payment paymentType READONLY
+    AS
     BEGIN
-		SET @Message = 'Valores con null';
-        RAISERROR('%s', 16, 1, @Message);
-        RETURN;
-    END;
-    SET @InicieTransaccion = 0;
+        SET NOCOUNT ON;
+        DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT;
+        DECLARE @Message VARCHAR(200);
+        DECLARE @InicieTransaccion BIT;
 
-    IF @@TRANCOUNT = 0
-    BEGIN
-		
-        SET @InicieTransaccion = 1;
-        SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-        BEGIN TRANSACTION;
-    END;
+        SET @InicieTransaccion = 0;
 
-    BEGIN TRY
-        SET @CustomError = 2001;
-        INSERT INTO dbo.contracts (startDate, endDate, contractStatusId)
-        SELECT startDate, endDate, contractStatusId
-        FROM @contracts;
-
-        IF @InicieTransaccion = 1
+        IF @@TRANCOUNT = 0
         BEGIN
-            COMMIT;
-        END;
-    END TRY
-    BEGIN CATCH
-        SET @ErrorNumber = ERROR_NUMBER();
-        SET @ErrorSeverity = ERROR_SEVERITY();
-        SET @ErrorState = ERROR_STATE();
-        SET @Message = ERROR_MESSAGE();
-
-        IF @InicieTransaccion = 1
-        BEGIN
-            ROLLBACK;
+            SET @InicieTransaccion = 1;
+            SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+            BEGIN TRANSACTION;
         END;
 
-        RAISERROR('%s - Error Number: %i', @ErrorSeverity, @ErrorState, @Message, @CustomError);
-    END CATCH;
-END;
+        BEGIN TRY
+            SET @CustomError = 2001;
+            
+            INSERT INTO [dbo].[invoices] ([date], [dueDate], [total_amount], [producerId], [number], [discountXsponsor])
+            SELECT paymentDate, dueDate, amount, producerId,  number, discount
+            FROM @payment;
+            UPDATE p
+            SET p.balance = p.balance - pt.amount
+            FROM producers p
+            INNER JOIN @payment pt ON p.producerId = pt.producerId;
 
-GO
+            IF @InicieTransaccion = 1
+            BEGIN
+                COMMIT;
+            END;
+        END TRY
+        BEGIN CATCH
+            SET @ErrorNumber = ERROR_NUMBER();
+            SET @ErrorSeverity = ERROR_SEVERITY();
+            SET @ErrorState = ERROR_STATE();
+            SET @Message = ERROR_MESSAGE();
 
-DECLARE @contracts AS ContractType;
-INSERT INTO @contracts (startDate, endDate, contractStatusId) 
-VALUES 
-    ('2023-04-01', '2024-03-31', 2), 
-    ('2023-05-01', '2023-08-31', 1);
+            IF @InicieTransaccion = 1
+            BEGIN
+                ROLLBACK;
+            END;
 
-EXEC [dbo].[InsertContract] @contracts;
-
-select * from dbo.contracts;
+            RAISERROR(''%s - Error Number: %i'', @ErrorSeverity, @ErrorState, @Message, @CustomError);
+        END CATCH;
+    END;')
+END
